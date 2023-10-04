6b94f54c9f8e3daa5e8d9339645c4a397e2c9052
LeYapson <theau.yapi@ynov.com>
2023-10-04 17:47:32 +0200

    trop fort wesh

--- Capitalize.cs
+++ Capitalize.cs
@@ -4,7 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static string Capitalize(string str)
         {
-            return "HELLOWORLD";
+            return str.ToUpper();
         }
     }
 }
