08289e965c3d95cdd2b5d6f7b6dfbb86eef35a54
LeYapson <theau.yapi@ynov.com>
2023-10-04 17:07:38 +0200

    trop fort wesh

--- SetPiValue.cs
+++ SetPiValue.cs
@@ -4,7 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static float SetPiValue(float pi)
         {
-            return pi+ 3.14159;  
+            return pi+ 3.14159f;  
         }
     }
 }
