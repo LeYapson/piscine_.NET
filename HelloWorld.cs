d7f18b6a853414bb1f52ecde8e44e0bc94323d09
LeYapson <theau.yapi@ynov.com>
2023-10-04 15:59:07 +0200

    inchallah la piscine commence

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,7 +4,7 @@ namespace CSharpDiscovery.Quest01
     {
         public static string HelloWorld()
         {
-            string hello = "Hello World!";
+            string hello = "Hello World !";
             return hello;
         }
     }
