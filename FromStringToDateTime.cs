f6ff396f9b4e6d1b77cd72f19239b72a4cf60bcc
LeYapson <theau.yapi@ynov.com>
2023-10-05 15:59:17 +0200

    trop fort wesh

---
+++ FromStringToDateTime.cs
@@ -0,0 +1,10 @@
+namespace CSharpDiscovery.Quest02
+{
+    public class FromStringToDateTime_Exercice
+    {
+        public static DateTime FromStringToDateTime(string dateStr)
+        {
+            return "21/12/2021 18:33:00";
+        }
+    }
+}
