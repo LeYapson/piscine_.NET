10a825bdcef51c3bea48b848cf5853d6c53125a8
LeYapson <theau.yapi@ynov.com>
2023-10-05 16:41:58 +0200

    trop fort wesh

---
+++ FindLastIndex.cs
@@ -0,0 +1,17 @@
+namespace CSharpDiscovery.Quest02
+{
+    public class FindLastIndex_Exercice
+    {
+        public static int? FindLastIndex(int[] tab, int a)
+        {
+            for (int i = tab.Length - 1; i >= 0; i--)
+            {
+                if (tab[i] == a)
+                {
+                    return i;
+                }
+            }
+            return null;
+        }
+    }
+}
