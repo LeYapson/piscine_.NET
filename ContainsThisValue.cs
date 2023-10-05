f8d22bedaa95ff9cf60d5d1d6e0d53d8b1018522
LeYapson <theau.yapi@ynov.com>
2023-10-05 16:24:11 +0200

    trop fort wesh

--- ContainsThisValue.cs
+++ ContainsThisValue.cs
@@ -1,15 +1,19 @@
+using System.Linq; // Required for the Contains method
+
 namespace CSharpDiscovery.Quest02
 {
     public class ContainsThisValue_Exercice
     {
         public static bool ContainsThisValue(int[] tab, int value)
         {
-            for (int i = 0; i < tab.Length; i++)
+            if (tab == null)
             {
-                if (tab[i] == value)  // Corrected this line
-                {
-                    return true;
-                }
+                return false;
+            }
+
+            if (tab.Contains(value))
+            {
+                return true;
             }
 
             return false;
