d503005557014a4e79337716788f9b0b6da65370
LeYapson <theau.yapi@ynov.com>
2023-10-07 10:28:00 +0200

    sa avance  ouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 
 namespace CSharpDiscovery.Quest03;
 
@@ -33,6 +34,38 @@ public class PointOfInterest
     {
         string encodedName = WebUtility.UrlEncode(Name);
         return string.Format(GoogleMapsUrlTemplate, encodedName, Latitude, Longitude);
+    }
+    
+    //Method to get distance
+    public int GetDistance(PointOfInterest other)
+    {
+        return (int)Math.Round(HaversineDistance(this.Latitude, this.Longitude, other.Latitude, other.Longitude));
+    }
+
+    public static int GetDistance(PointOfInterest p1, PointOfInterest p2)
+    {
+        return (int)Math.Round(HaversineDistance(p1.Latitude, p1.Longitude, p2.Latitude, p2.Longitude));
+    }
+
+    private static double HaversineDistance(double lat1, double lon1, double lat2, double lon2)
+    {
+        const double R = 6371.0; // Radius of the Earth in kilometers
+        var lat1Rad = DegreesToRadians(lat1);
+        var lat2Rad = DegreesToRadians(lat2);
+        var deltaLat = DegreesToRadians(lat2 - lat1);
+        var deltaLon = DegreesToRadians(lon2 - lon1);
+
+        var a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
+                Math.Cos(lat1Rad) * Math.Cos(lat2Rad) * 
+                Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        return R * c;
+    }
+
+    private static double DegreesToRadians(double degrees)
+    {
+        return degrees * (Math.PI / 180);
     }
     
     // Override the ToString method
