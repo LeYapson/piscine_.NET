57dc4b401633b08536b8b1a04ac076fd09ac25f4
LeYapson <theau.yapi@ynov.com>
2023-10-07 10:49:17 +0200

    sa avance  ouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace CSharpDiscovery.Quest04;
+
+public class Car : Vehicule
+{
+    public string Model { get; set; }
+
+    public Car() : base()
+    {
+        Model = "Unknown";
+    }
+    
+    // Constructor with Model, Brand, Color, and CurrentSpeed
+    public Car(string model, string brand, string color, int currentSpeed = 0) 
+        : base(brand, color, currentSpeed)
+    {
+        Model = model;
+    }
+
+    public override string ToString()
+    {
+        return $"{Color} {Brand} {Model}";
+    }
+    
+}
