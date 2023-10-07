e455db6fdc7b2a68c1cad108bc139b2e9a32926b
LeYapson <theau.yapi@ynov.com>
2023-10-07 10:40:13 +0200

    sa avance  ouuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu

---
+++ HistoricalMonument.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest03
+{
+    public class HistoricalMonument : PointOfInterest
+    {
+        public int BuildYear { get; set; }
+
+        // Empty constructor
+        public HistoricalMonument()
+        {
+        }
+
+        // Constructor with Latitude, Longitude, Name, and BuildYear
+        public HistoricalMonument(double latitude, double longitude, string name, int buildYear) 
+            : base(name, latitude, longitude)
+        {
+            BuildYear = buildYear;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} is a historical monument built in {BuildYear}";
+        }
+    }
+}
