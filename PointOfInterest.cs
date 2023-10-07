be2a092a501198669d73313dc1296588392ab127
LeYapson <theau.yapi@ynov.com>
2023-10-07 10:09:18 +0200

    on continu csharp ouuuuu

---
+++ PointOfInterest.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharpDiscovery.Quest03;
+
+public class PointOfInterest
+{
+    // Properties
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string? Name { get; set; }  // Nullable string
+
+    // Static property
+    public static string GoogleMapsUrlTemplate { get; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";   
+}
